7ed8c3298f586690246d1002db8d03a9163cf61f
Fesleep <gomesluizfelipe36@gmail.com>
2020-08-31 21:19:36 -0300

    Adicionados textos

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -7,6 +7,10 @@ namespace TesteGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Good Morning");
+            Console.WriteLine("TESTE");
+            Console.WriteLine("alsfpdla");
+            Console.WriteLine("pdgasg");
         }
     }
 }
